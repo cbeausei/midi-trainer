d1f6f1bd205b7c7649ee4fb0d47eab1467844490
cbeausei <clementbeauseigneur@gmail.com>
2021-03-27 23:07:51 +0100

    Make service worker network-first

--- app/sw.js
+++ app/sw.js
@@ -10,23 +10,25 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open('offline').then(cache => {
-      return cache.match(event.request);
-    }).then(response => {
+  event.respondWith(new Promise(async (resolve) => {
+    const cache = await caches.open('offline');
+    try {
+      const response = await fetch(event.request);
+      const responseClone = response.clone();
+      cache.put(event.request, responseClone);
+      resolve(response);
+    } catch {
+      const response = await cache.match(event.request);
       if (response !== undefined) {
-        return response;
+        resolve(response);
       } else {
-        return fetch(event.request).then(response => {
-          let responseClone = response.clone();
-          caches.open('offline').then(cache => {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(() => {
-          return new Response('No network & cache failed.');
-        });
+        const offlineResponse = await cache.match('offline.html');
+        if (offlineResponse !== undefined) {
+          resolve(offlineResponse);
+        } else {
+          resolve(new Response('No network & cache failed.'));
+        }
       }
-    })
-  );
+    }
+  }));
 });
